1b13634c753c30e30ec5256da7a32e081ad6c1c6
Torelina <nataneri@mail.ru>
2020-09-20 23:14:13 +0300

    Добавлена конструкция

---
+++ Database/Database connection/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_connection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String ConSt = "Data Source=tsoop.c6oo9thiejqr.us-east-1.rds.amazonaws.com;Initial" +
+                      " Catalog=TSOOPEX;User ID=admin;Password=Geirby1799";
+            SqlConnection conection = new SqlConnection(ConSt);
+            conection.Open();
+
+
+            conection.Close();
+        }
+    }
+}
